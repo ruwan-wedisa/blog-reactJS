cc7e7225c1973dcf4000cb7961c91ea6c87323ce
ruwan.wedisa0150@gmail.com <you@example.com>
2019-02-21 00:17:10 +0530

    axios dummy json posts

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () =>{
     return(
         <div className="container">
             <h4 className="center">About</h4>
-            <p>Irure incididunt do ad cupidatat eu labore non ut tempor aliqua minim deserunt.</p>
+            <p>Irure incididunt do ad cupidatat eu labore non ut tempor aliqua minim deserunt.Irure incididunt do ad cupidatat eu labore non ut tempor aliqua minim deserunt.</p>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React,{Component} from 'react';
+import axios from 'axios';
 
-const Home = () =>{
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Irure incididunt do ad cupidatat eu labore non ut tempor aliqua minim deserunt.</p>
-        </div>
-    )
+class Home extends Component{
+    state={
+        posts:[]
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res =>{
+            this.setState({
+                posts:res.data.slice(0,10)
+            })
+        })
+    }
+    render(){
+        const {posts} = this.state;
+        const postList = posts.length ?(
+            posts.map(post =>{
+                return(
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        )
+        
+        :(
+            <div className="center">No Posts Yet</div>
+        )
+
+        return(
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home;
