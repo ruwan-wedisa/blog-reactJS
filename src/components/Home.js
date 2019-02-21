38664df8dab51cf8a353f973a0fff2a75122a3db
ruwan.wedisa0150@gmail.com <you@example.com>
2019-02-21 11:16:54 +0530

    add image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import PokeBall from '../pokeball.png'
+
 class Home extends Component{
     state={
         posts:[]
@@ -19,6 +21,7 @@ class Home extends Component{
             posts.map(post =>{
                 return(
                     <div className="post card" key={post.id}>
+                        <img src={PokeBall} alt="A poke Ball"/>
                         <div className="card-content">
                             <Link to={'/'+post.id}>
                             <span className="card-title">{post.title}</span>
@@ -35,7 +38,7 @@ class Home extends Component{
         )
 
         return(
-            <div className="container">
+            <div className="container home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
